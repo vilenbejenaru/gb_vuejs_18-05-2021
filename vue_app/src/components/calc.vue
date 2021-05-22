83274f6fec676b462a564534677f6a7d284ee2a7
Vilen <vilen.bejenaru@wakatech.ro>
2021-05-23 02:14:14 +0300

    finish lesson2

--- vue_app/src/components/calc.vue
+++ vue_app/src/components/calc.vue
@@ -10,13 +10,23 @@
       {{errorText}}
     </div>
     <div class="btns">
-      <button class='btn btn__qwerty' @click="calc('+')">a+b</button>
-      <button class='btn' @click="calc('-')">a-b</button>
-      <button class='btn' v-on:click="calc('*')">a*b</button>
-      <button class='btn' @click="calc('/')">a/b</button>
-      <button class='btn' @click="calc('**')">a**b</button>
-      <button class='btn' @click="calc('%')">a%b</button>
+      <button class='btn' v-for="(operation, idx) in buttons"
+      :key="idx"
+      @click="calc(operation)"
+      :title="operation"
+      :disabled="op1 === '' && op2 === '' "> {{operation}} </button>
     </div>
+    <br>
+    <div class="logs">
+      Последние вычисления: <br>{{logs}}
+    </div>
+    <div class="result">
+      <div class="cond" v-if="result > 0">Результат положительный</div>
+      <div class="cond" v-else-if="result < 0">Результат отрицательный</div>
+      <div class="cond" v-else>Результат = 0 </div>
+    </div>
+    <div class="numbers" v-for="(item, idx) in numbersComp" :key="idx"> {{item}} </div>
+    <div class="fibo">Фибоначчи: {{fibResult}}</div>
     <hr />
   </div>
 </template>
@@ -37,10 +47,22 @@ export default {
       op2: '',
       result: 0,
       hasError: false,
-      errorText: ' На ноль делить нельзя!!!'
+      errorText: ' На ноль делить нельзя!!!',
+      buttons: ['+', '-', '*', '/', '**', '%'],
+      numbers: [1, 2, 3, 5, 6, 7, 8, 11, 2, 3, 5],
+      logs: {}
+    }
+  },
+  watch: {
+    op2 (newValue) {
+      console.log(newValue)
+      this.result = this.op1 + newValue
     }
   },
   methods: {
+    fib (n) {
+      return n <= 1 ? n : this.fib(n - 1) + this.fib(n - 2)
+    },
     calc (operation) {
       this.hasError = false
       if (operation === '/' && this.op2 === 0) {
@@ -49,6 +71,9 @@ export default {
       }
       const { op1, op2 } = this
       // const op1 = this.op1
+      this.$set(this.logs, Date.now(), `${op1} ${operation} ${op2} = ?`)
+      // this.logs[Date.now()] = `${op1} ${operation} ${op2}=?` без реактивности
+
       const calcOperations = {
         '+': () => op1 + op2,
         '-': () => op1 - op2,
@@ -58,6 +83,17 @@ export default {
         '%': () => op1 % op2
       }
       this.result = calcOperations[operation]()
+    }
+  },
+  computed: {
+    numbersComp () {
+      return this.numbers.filter(n => n > 6)
+    },
+    fib1 () {
+      return this.fib(this.op1)
+    },
+    fibResult () {
+      return this.fib(this.op1) - this.fib(this.op2)
     }
   }
 }
@@ -93,7 +129,7 @@ input {
 }
 .btn {
   background-color: #42b983;
-  border: 2px solid 35495e;
+  border: 2px solid #35495e;
   border-radius: 20px;
   width: 80px;
   height: 40px;
@@ -107,5 +143,18 @@ input {
     background-color: yellow;
     border: 3px solid red;
     padding: 10px;
+  }
+  .cond {
+    font-size: 24px;
+    color: #35495e;
+  }
+  .numbers {
+    font-size: 24px;
+    color: #35495e;
+  }
+  .logs, .fibo {
+  border: 1px solid #35495e;
+  margin: 20px 0;
+  padding: 10px;
   }
 </style>
