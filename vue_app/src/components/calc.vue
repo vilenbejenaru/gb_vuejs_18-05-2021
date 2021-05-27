c877998d8a6b247f1331d1e288a36783ca4e611c
Vilen <vilen.bejenaru@wakatech.ro>
2021-05-27 15:12:35 +0300

    lesson2 homework

--- vue_app/src/components/calc.vue
+++ vue_app/src/components/calc.vue
@@ -1,106 +1,144 @@
 <template>
-  <div class='calc'>
-    <div class="inputs">
-      <input placeholder='a' v-model.number.lazy="op1" />
-      <input placeholder='b' v-model.number.trim="op2" />
-      <span class='result'> = {{ result }} </span>
+  <div>
+    <div class="calc">
+      <div class="inputs">
+        <input placeholder="a" v-model.number.lazy="op1" />
+        <input placeholder="b" v-model.number.trim="op2" />
+        <span class="result"> = {{ result }} </span>
+      </div>
+      <br />
+      <div class="error" v-if="hasError">
+        {{ errorText }}
+      </div>
+      <div class="btns">
+        <button
+          class="btn"
+          v-for="(operation, idx) in buttons"
+          :key="idx"
+          @click="calc(operation)"
+          :title="operation"
+          :disabled="op1 === ' ' && op2 === ' '"
+        >
+          {{ operation }}
+        </button>
+      </div>
+      <hr />
+      <input
+        type="checkbox"
+        id="checkbox"
+        @click="checked = !checked"
+        v-model="checked"
+      />
+      <label for="checkbox">Цифровая Клавиатура</label>
+      <div v-if="checked">
+        <keyboard @addNum="addNumber" @delNum="delNumber" />
+      </div>
     </div>
-    <br />
-    <div class="error"  v-if="hasError">
-      {{errorText}}
+    <div class="results">
+      <div class="logs">Последние вычисления: <br />{{ logs }}</div>
+      <div class="result">
+        <div class="cond" v-if="result > 0">Результат положительный</div>
+        <div class="cond" v-else-if="result < 0">Результат отрицательный</div>
+        <div class="cond" v-else>Результат = 0</div>
+      </div>
+      <div class="numbers" v-for="(item, idx) in numbersComp" :key="idx">
+        {{ item }}
+      </div>
+      <div class="fibo">Фибоначчи: {{ fibResult }}</div>
     </div>
-    <div class="btns">
-      <button class='btn' v-for="(operation, idx) in buttons"
-      :key="idx"
-      @click="calc(operation)"
-      :title="operation"
-      :disabled="op1 === '' && op2 === '' "> {{operation}} </button>
-    </div>
-    <br>
-    <div class="logs">
-      Последние вычисления: <br>{{logs}}
-    </div>
-    <div class="result">
-      <div class="cond" v-if="result > 0">Результат положительный</div>
-      <div class="cond" v-else-if="result < 0">Результат отрицательный</div>
-      <div class="cond" v-else>Результат = 0 </div>
-    </div>
-    <div class="numbers" v-for="(item, idx) in numbersComp" :key="idx"> {{item}} </div>
-    <div class="fibo">Фибоначчи: {{fibResult}}</div>
-    <hr />
   </div>
 </template>
 
 <script>
+import keyboard from "./keyboard.vue";
 export default {
-  name: 'Calculator',
+  components: { keyboard },
+  name: "Calculator",
   props: {
     msg: String,
     secondMsg: {
       type: String,
-      default: 'User'
+      default: "User"
     }
   },
-  data () {
+  data() {
     return {
-      op1: '',
-      op2: '',
+      op1: "",
+      op2: "",
       result: 0,
       hasError: false,
-      errorText: ' На ноль делить нельзя!!!',
-      buttons: ['+', '-', '*', '/', '**', '%'],
+      errorText: " На ноль делить нельзя!!!",
+      buttons: ["+", "-", "*", "/", "**", "%"],
       numbers: [1, 2, 3, 5, 6, 7, 8, 11, 2, 3, 5],
-      logs: {}
-    }
+      logs: {},
+      checked: false
+    };
   },
   watch: {
-    op2 (newValue) {
-      console.log(newValue)
-      this.result = this.op1 + newValue
+    op2(newValue) {
+      this.result = this.op1 + newValue;
     }
   },
   methods: {
-    fib (n) {
-      return n <= 1 ? n : this.fib(n - 1) + this.fib(n - 2)
-    },
-    calc (operation) {
-      this.hasError = false
-      if (operation === '/' && this.op2 === 0) {
-        this.hasError = true
-        return
+    addNumber(val, op) {
+      if (op == 2) {
+        let tempVal = this.op2;
+        tempVal = tempVal + val;
+        this.op2 = Number(val);
+      } else {
+        let tempVal = this.op1;
+        tempVal = tempVal + val;
+        this.op1 = Number(val);
       }
-      const { op1, op2 } = this
+    },
+    delNumber(op) {
+      if (op == 2) {
+        this.op2 = "";
+      } else {
+        this.op1 = "";
+      }
+    },
+    calc(operation) {
+      this.hasError = false;
+      if (operation === "/" && this.op2 === 0) {
+        this.hasError = true;
+        return;
+      }
+      const { op1, op2 } = this;
       // const op1 = this.op1
-      this.$set(this.logs, Date.now(), `${op1} ${operation} ${op2} = ?`)
+      this.$set(this.logs, Date.now(), `${op1} ${operation} ${op2} = ?`);
       // this.logs[Date.now()] = `${op1} ${operation} ${op2}=?` без реактивности
 
       const calcOperations = {
-        '+': () => op1 + op2,
-        '-': () => op1 - op2,
-        '*': () => op1 * op2,
-        '/': () => op1 / op2,
-        '**': () => op1 ** op2,
-        '%': () => op1 % op2
-      }
-      this.result = calcOperations[operation]()
+        "+": () => op1 + op2,
+        "-": () => op1 - op2,
+        "*": () => op1 * op2,
+        "/": () => op1 / op2,
+        "**": () => op1 ** op2,
+        "%": () => op1 % op2
+      };
+      this.result = calcOperations[operation]();
+    },
+    fib(n) {
+      return n <= 1 ? n : this.fib(n - 1) + this.fib(n - 2);
     }
   },
   computed: {
-    numbersComp () {
-      return this.numbers.filter(n => n > 6)
+    numbersComp() {
+      return this.numbers.filter(n => n > 6);
     },
-    fib1 () {
-      return this.fib(this.op1)
+    fib1() {
+      return this.fib(this.op1);
     },
-    fibResult () {
-      return this.fib(this.op1) - this.fib(this.op2)
+    fibResult() {
+      return this.fib(this.op1) - this.fib(this.op2);
     }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style>
 h3 {
   margin: 40px 0 0;
 }
@@ -123,6 +161,10 @@ input {
   padding: 10px;
   box-sizing: border-box;
 }
+label {
+  font-size: 30px;
+  color: #35495e;
+}
 .result {
   font-size: 36px;
   color: blue;
@@ -135,26 +177,24 @@ input {
   height: 40px;
   font-size: 24px;
   margin: 5px;
-  &__qwerty {
-    color: #fff;
-  }
- }
-  .error {
-    background-color: yellow;
-    border: 3px solid red;
-    padding: 10px;
-  }
-  .cond {
-    font-size: 24px;
-    color: #35495e;
-  }
-  .numbers {
-    font-size: 24px;
-    color: #35495e;
-  }
-  .logs, .fibo {
+}
+.error {
+  width: 120px;
+  background-color: yellow;
+  border: 3px solid red;
+}
+.cond {
+  font-size: 24px;
+  color: #35495e;
+}
+.numbers {
+  font-size: 24px;
+  color: #35495e;
+}
+.logs,
+.fibo {
   border: 1px solid #35495e;
   margin: 20px 0;
   padding: 10px;
-  }
+}
 </style>
---
+++ vue_app/src/components/keyboard.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <div class="key-btns">
+      <button
+        class="btn key__btn"
+        v-for="(btn, index) in keyBtns"
+        :key="index"
+        @click="addNum(btn.value)"
+      >
+        {{ btn.value }}
+      </button>
+    </div>
+    <div class="key-action">
+      <div class="key-action__inputs">
+        <input
+          class="key-action__input"
+          type="radio"
+          id="op1"
+          value="1"
+          v-model="picked"
+        />
+        <label for="op1">Операнд A</label>
+        <input
+          class="key-action__input"
+          type="radio"
+          id="op2"
+          value="2"
+          v-model="picked"
+        />
+        <label for="op2">Операнд B</label>
+      </div>
+      <button @click="delNum()" class="btn error">Delete</button>
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      keyBtns: {
+        0: {
+          name: "0",
+          value: 0
+        },
+        1: {
+          name: "1",
+          value: 1
+        },
+        2: {
+          name: "2",
+          value: 2
+        },
+        3: {
+          name: "3",
+          value: 3
+        },
+        4: {
+          name: "4",
+          value: 4
+        },
+        5: {
+          name: "5",
+          value: 5
+        },
+        6: {
+          name: "6",
+          value: 6
+        },
+        7: {
+          name: "7",
+          value: 7
+        },
+        8: {
+          name: "8",
+          value: 8
+        },
+        9: {
+          name: "9",
+          value: 9
+        }
+      },
+      picked: 1
+    };
+  },
+  methods: {
+    addNum(value) {
+      this.$emit("addNum", value, this.picked);
+    },
+    delNum() {
+      this.$emit("delNum", this.picked);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.del__btn {
+  width: 120px;
+}
+.key-btns {
+  margin: 50px;
+}
+.key__btn {
+  width: 50px;
+}
+.key-action {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 20px auto;
+}
+.key-action__input {
+  width: 50px;
+  height: 20px;
+  margin: 20px;
+}
+</style>
