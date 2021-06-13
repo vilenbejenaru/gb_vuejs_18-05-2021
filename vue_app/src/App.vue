65385f3fb8a746a5aec376ecd0603a56571202cd
Vilen <vilen.bejenaru@wakatech.ro>
2021-06-13 13:59:39 +0300

    hashed url

--- vue_app/src/App.vue
+++ vue_app/src/App.vue
@@ -2,9 +2,17 @@
 	<div id="app">
 		<div class="wrapper">
 			<header>
-				<h1 class="title">My personal Cost</h1>
-				<h2 class="subtitle">Total: {{ getFullPaymentValue  }}</h2>
+				<a href="#dashboard">Dashboard</a>/ <a href="#about">About</a>/
+				<a href="#unknown">404</a>
+				<div class="pages">
+					<page-dash-board v-if="page === 'dashboard'" />
+					<page-about v-else-if="page == 'about'" />
+					<page-404 v-else />
+				</div>
 			</header>
+			<h1 class="title">My personal Cost</h1>
+			<h2 class="subtitle">Total: {{ getFullPaymentValue }}</h2>
+
 			<main>
 				<add-payment
 					v-if="addBtnIsShown"
@@ -28,48 +36,55 @@
 	import { mapGetters, mapMutations, mapActions } from 'vuex';
 	import PaymentsDisplay from './components/PaymentsDisplay.vue';
 	import AddPayment from './components/addPayment.vue';
+	import PageDashBoard from './views/PageDashBoard.vue';
+	import Page404 from './views/Page404.vue';
+	import PageAbout from './views/PageAbout.vue';
 
 	export default {
 		name: 'App',
 		components: {
 			PaymentsDisplay,
 			AddPayment,
+			PageDashBoard,
+			Page404,
+			PageAbout,
 		},
 
-data() {
-    return {
-		addBtnIsShown: false
-    }
-  },
-  actions: {
-    ...mapActions([
-      'fetchData'
-    ])
-  },
-  computed: {
-    ...mapGetters([
-      'getPaymentList',
-      'getFullPaymentValue'
-    ]),
-  },
-  created() {
-    // this.setPaymentListData(this.fetchData())
-  },
-  mounted(){
-    this.$store.dispatch('fetchData')
-  },
-  methods: {
-    ...mapMutations([
-      'setPaymentListData'
-    ]),
-    emitAction() {
-      this.addBtnIsShown = false;
-    },
-    addPayment(data) {
-      this.$store.commit('addDataToPaymentList', data)
-    }
-  }
-};
+		data() {
+			return {
+				addBtnIsShown: false,
+				page: ' ',
+			};
+		},
+		actions: {
+			...mapActions(['fetchData']),
+		},
+		computed: {
+			...mapGetters(['getPaymentList', 'getFullPaymentValue']),
+		},
+		created() {
+			// this.setPaymentListData(this.fetchData())
+		},
+		mounted() {
+			this.$store.dispatch('fetchData');
+			this.setPage();
+			window.addEventListener('hashchange', () => {
+				this.setPage();
+			});
+		},
+		methods: {
+			...mapMutations(['setPaymentListData']),
+			emitAction() {
+				this.addBtnIsShown = false;
+			},
+			addPayment(data) {
+				this.$store.commit('addDataToPaymentList', data);
+			},
+			setPage() {
+				this.page = location.pathname.slice(1);
+			},
+		},
+	};
 </script>
 
 <style lang="scss" scoped>
---
+++ vue_app/src/views/PageDashBoard.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<h1>DashBoard</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'PageDashBoard',
+	};
+</script>
+
+<style lang="scss" scoped></style>
