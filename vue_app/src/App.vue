14e3fcc080074204241654987360ec3b92a72d20
Vilen <vilen.bejenaru@wakatech.ro>
2021-06-18 14:37:17 +0300

    lesson6 begin; update project

--- vue_app/src/App.vue
+++ vue_app/src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="wrapper">
+            <header>
+                <router-link to="/dashboard"> Dashboard R </router-link>/
+                <router-link to="/about"> About R </router-link>/
+                <router-link to="/page404"> 404 R </router-link> /
+            </header>
+            <h1 class="title">My personal Cost</h1>
+            <h2 class="subtitle">Total: {{ getFullPaymentValue }}</h2>
+            <main>
+                <router-view />
+            </main>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapMutations } from 'vuex';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    data() {
+        return {
+            page: '',
+        };
+    },
+    mounted() {
+        this.$store.dispatch('fetchData');
+    },
+    methods: {
+        ...mapMutations(['setPaymentListData']),
+        emitAction() {
+            this.addBtnIsShown = false;
+        },
+        addPayment(data) {
+            this.$store.commit('addDataToPaymentList', data);
+        },
+        goTopage(pageName) {
+            this.$router.push({
+                name: pageName,
+            });
+        },
+    },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
